6b7a9091efefb4efccbab52430fb9170a5f88b5f
ldavis84 <ldavis@sazetech.com>
2017-03-29 17:41:49 -0400

    Add files via upload

--- elemGainLoad.m
+++ elemGainLoad.m
@@ -1,9 +1,11 @@
 function elemGainLoad(Glook,Ellook,Azlook,lamlook,modelook)
-% (Glook,Ellook,Azlook,lamlook,modelook);
+% elemGainLoad(Glook,Ellook,Azlook,lamlook,modelook);
 % Loads variables into the global area for future use by elemGainLookup.
 % Supports interpolation of values and wavelengths for either one of two
 % polarizations.  After calling this routine, one then uses 
 % @elemGainLookup as the function for arrayAthley and other codes.
+% Values of azimuth, elevation, and wavelength should be monotonically
+% increasing, but do not need to be evenly spaced.
 % Glook        -- nEl x nAz x nelem x nlam x npol stored lookup tables for
 %                 antenna elements
 % Ellook       -- nEl x 1 reference elevation values, deg
@@ -18,11 +20,11 @@ function elemGainLoad(Glook,Ellook,Azlook,lamlook,modelook)
 %                 better for antenna gains because it linearizes phase and
 %                 accommodates widely varying gains better.
 
-global Gref AzRef ElRef lamRef modeInterp
+global Gref AzRef ElRef lamRef modeInterp AzHi
 
 % check dimensions
 
-[nEl,nAz,~,nlam] = size(Glook);
+[nEl,nAz,~,nlam,~] = size(Glook);
 
 Ellook = Ellook(:);
 
@@ -40,12 +42,26 @@ end
 
 if (nlam > 1)
     lamlook = lamlook(:);
+    [lamlook,ilook] = sort(lamlook,'ascend');   % check order
+    
+    if any(ilook ~= (1:nlam)')
+        Glook = Glook(:,:,:,ilook,:);
+    end
     
     if (length(lamlook) ~= nlam)
         fprintf(['elemGainLoad:  lookup table does not ',...
             'match #lam values!!\n']);
         return;
     end
+end
+
+% check angle ranges, find median azimuth and range
+
+AzHi = 0.5*(Azlook(nAz) + Azlook(1)) + 180;    % top end of mapping range
+AzRange = (Azlook(nAz) - Azlook(1));
+
+if (AzRange < 360)
+    fprintf('elemGainLoad:  azimuth range is %0.f < 360 deg!\n',AzRange);
 end
 
 Gref = Glook;
